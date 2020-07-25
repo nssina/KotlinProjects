030006a780527a508e63c20123a712248d249d5d
nsdevelopteam <rabie_sina@yahoo.com>
2020-07-25 05:10:04 -0400

    Added FoodApp

---
+++ RestaurantMenu/app/src/main/java/com/example/restaurantmenu/Food.kt
@@ -0,0 +1,13 @@
+package com.example.restaurantmenu
+
+class Food {
+    var name: String? = null
+    var des: String? = null
+    var image: Int? = null
+
+    constructor(nam: String, des: String, image: Int) {
+        this.name = name
+        this.des = des
+        this.image = image
+    }
+}
