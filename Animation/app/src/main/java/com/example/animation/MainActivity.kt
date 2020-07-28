f7a5ab8c730126bfa080d2eb8cfc59fe70733f45
nsdevelopteam <rabie_sina@yahoo.com>
2020-07-28 10:19:54 -0400

    Overlay animation added

--- Animation/app/src/main/java/com/example/animation/MainActivity.kt
+++ Animation/app/src/main/java/com/example/animation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var image = findViewById<ImageView>(R.id.animation_image)
+        var overlay = findViewById<LinearLayout>(R.id.overlay_background)
+
+
         var backgroundAnimation = image.background as AnimationDrawable
         backgroundAnimation.setEnterFadeDuration(3000)
         backgroundAnimation.setExitFadeDuration(1000)
         backgroundAnimation.start()
+
+        var overLay = overlay.background as AnimationDrawable
+        overLay.setEnterFadeDuration(3000)
+        overLay.setExitFadeDuration(1000)
+        overLay.start()
+
     }
 }
