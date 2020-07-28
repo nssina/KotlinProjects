f2303bc588d73bc0a5d8b2e204622ce18970254e
nsdevelopteam <rabie_sina@yahoo.com>
2020-07-28 10:00:50 -0400

    Added animation and everything working fine =))

--- Animation/app/src/main/java/com/example/animation/MainActivity.kt
+++ Animation/app/src/main/java/com/example/animation/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.animation
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        var image = findViewById<ImageView>(R.id.animation_image)
+        var backgroundAnimation = image.background as AnimationDrawable
+        backgroundAnimation.setEnterFadeDuration(3000)
+        backgroundAnimation.setExitFadeDuration(1000)
+        backgroundAnimation.start()
     }
 }
